089eb9ad00c6682e6658c623515fe86da8d309b1
André Roberge <andre.roberge@gmail.com>
2017-12-29 12:55:11 -0400

    automatic update

--- build/reeborg.js
+++ build/reeborg.js
@@ -1514,13 +1514,13 @@ function draw_grid_walls (ctx, edit){
        drawing context.
        If may also be called to draw on the goal drawing context (above the tile)
        if we are editing the world **or** if RUR.state.visible_grid evaluates
-       as **equivalent** to true.
+       to RUR.PATH_ONLY.
        
-       If RUR.state.visible_grid is **equal** to true
+       If RUR.state.visible_grid is equal to RUR.PATH_ONLY
        and a desired path named RUR.public.path has been defined, then we only
        draw the grid on that desired path.
 
-       If RUR.state.visible_grid is **equivalent** to true but not **equal** 
+       If RUR.state.visible_grid is true but not equal to RUR.PATH_ONLY 
        OR if RUR.public.path is not defined 
        (or is not used for something that can be treated as
        as path below, raising an Error), 
@@ -1529,7 +1529,7 @@ function draw_grid_walls (ctx, edit){
 
     draw_only_path = false;
     if (!edit && // always draw when edit
-        RUR.state.visible_grid === true &&  // not simply equivalent to true
+        RUR.state.visible_grid == RUR.PATH_ONLY && 
         RUR.public !== undefined && // should always be the case
         RUR.public.path !== undefined) { // world creator appears to have created a desired path
             draw_only_path = true;
@@ -8755,6 +8755,16 @@ RUR.RED = "red";     // RUR.configure_red_green() below
 ==========================================================*/
 
 RUR.SHOW_EDITORS_CONTENTS = false;
+
+/*=========================================================
+  Grid visibility when background tiles or background image
+  would other wise hide it.
+
+  This would be a value assigned to RUR.state.visible_grid
+==========================================================*/
+
+RUR.ALL_GRID_VISIBLE = 1;
+RUR.PATH_ONLY = "grid_visible_on_path_only";
 
 /*========================================================
   Some initial defaults
